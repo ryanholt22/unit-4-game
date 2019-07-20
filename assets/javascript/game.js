aec8c9178ece0964da69ae0318136c5254524778
Ryan Holt <ryanholt22@yahoo.com>
2019-07-20 01:04:31 -0500

    did some psuedo coding!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,7 @@
 /*psuedo code brah 
 so, we know we that the user has to click one of the four images.
 we also know were going to need to make each image generate a different number.
+we also need another number generated.
+we will make the first generated number anywhere from 300-700
+each image will be able to generated from 0-500
+we need for and else statements for example matching the your score and the random number.
