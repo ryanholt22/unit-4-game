efa4fb1e4e7c93c983fca0942caeddab13444ac3
Ryan Holt <ryanholt22@yahoo.com>
2019-07-22 16:31:32 -0500

    made some js adjustments, made a few global vars.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,19 @@
 /*psuedo code brah 
 so, we know we that the user has to click one of the four images.
 we also know were going to need to make each image generate a different number.
-we also need another number generated.
-we will make the first generated number anywhere from 300-700
-each image will be able to generated from 0-500
-we need for and else statements for example matching the your score and the random number.
+we also need another number generated for the score youre trying to beat.
+we will make the first generated number (the score youre trying to beat) anywhere from 400-700 points 
+each image will be able to generated from 0-250 points
+we need for and else statements for example matching the your score and the random number
+if your number matches or is above the first random generated number then we will add a point to the win, if not we add it to the loss */
+//mathResults is the first number we generate.
+
+var randomResult;
+var win = 0;
+var lost = 0;
+
+
+for(var i = 0; i < 400; i++) {
+    var i = lost;
+    console.log("you lost!");
+}
