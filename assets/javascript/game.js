abc330699ec30ab36ed825ed0d905fe3c67fb1fe
Ryan Holt <ryanholt22@yahoo.com>
2019-07-22 23:33:45 -0500

    made some js changes. added a bunch.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,7 @@
 /*psuedo code brah 
 so, we know we that the user has to click one of the four images.
 we also know were going to need to make each image generate a different number.
-we also need another number generated for the score youre trying to beat.
-we will make the first generated number (the score youre trying to beat) anywhere from 400-700 points 
+we will make the first generated number (the score youre trying to beat) 500 points 
 each image will be able to generated from 0-220 points
 we need for and else statements for example matching the your score and the random number
 if your number matches or is above the first random generated number then we will add a point to the win, if not we add it to the loss */
@@ -16,17 +15,24 @@ var numTwo = Math.floor(Math.random() * 220);
 var numThree = Math.floor(Math.random() * 220);
 var numFour = Math.floor(Math.random() * 220);
 var numTotal = numOne + numTwo + numThree + numFour;
+var STB = 500;
 
 console.log(numOne);
 console.log(numTwo);
 console.log(numThree);
 console.log(numFour);
 console.log(numTotal);
+console.log(numTotal);
 
-    var imgOne = $("<img>");
-        imgOne.attr({
-            "class": 'imgOne',
-            "data-random": numOne
-        });
-        $("#cltwo").append(imgOne);
-        console.log($0);
+    var numOne = document.getElementById('#imgone');
+    var numTwo = document.getElementById('#imgtwo');
+    var numThree = document.getElementById('#imgthree');
+    var numFour = document.getElementById('#imgfour');
+    var numTotal = document.getElementById('#ytsi');
+    
+    if (numTotal > STB) {
+        win++
+        console.log(win);
+    };
+
+ 
