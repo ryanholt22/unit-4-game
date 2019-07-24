203285c166757efbde312b375c590f579328372c
Ryan Holt <ryanholt22@yahoo.com>
2019-07-23 23:21:17 -0500

    not finished but made lots of progress, had to redo almost the whole game because i didnt read the instructions right.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,12 +10,13 @@ if your number matches or is above the first random generated number then we wil
 var randomResult;
 var win = 0;
 var lost = 0;
-var numOne = Math.floor(Math.random() * 220);
-var numTwo = Math.floor(Math.random() * 220);
-var numThree = Math.floor(Math.random() * 220);
-var numFour = Math.floor(Math.random() * 220);
+var numOne = 2;
+var numTwo = 5;
+var numThree = 7;
+var numFour = 10;
 var numTotal = numOne + numTwo + numThree + numFour;
-var STB = Math.floor(Math.random()*1000);
+var total = 0;
+var STB = 500;
 
 console.log(numOne);
 console.log(numTwo);
@@ -32,35 +33,38 @@ console.log("total", numTotal);
 
 $(document).ready(function () {
 
+
+    $('.Wins').text("wins: " + win);
+    $('.Losses').text("losses: " + lost);
+    console.log(win);
+    console.log(lost)
     
 
     $('.imgone').on("click", function () {
-        alert("The number for this crystal is: " + numOne);
+        $('.totalScore').text('totalScore' + numOne);
+        alert("This Crystal Value Is: " + numOne);
     });
 
     $('.imgtwo').on("click", function () {
-        alert("The number for this picture is:" + " " + numTwo);
+        $('.totalScore').text('totalScore' + numTwo);
+        alert("This Crystal Value Is: " + numTwo);
     });
 
     $('.imgthree').on("click", function () {
-        alert("The number for this crystal is:" + " " + numThree);
+        $('.totalScore').text('totalScore' + numThree);
+        alert("This Crystal Value Is:: " + numThree);
     });
 
     $('.imgfour').on("click", function () {
-        alert("The number for this crystal is:" + " " + numFour);
+        $('.totalScore').text('totalScore' + numFour);
+        alert("This Crystal Value Is: " + numFour);
     });
 
-    $("#totalScore").text(numTotal);
+    if (total >= STB) {
+        win++
+    } else {
+        lost++
+    }
+    console.log("real total " + total);
 
 });
-
-
-
-
-//work here and below, trying to get anything to work.
-
-
-
-console.log(win);
-console.log(lost)
-
